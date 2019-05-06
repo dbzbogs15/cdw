c5a45b69266d03f045bd6dbe9aa8feeff3734f93
dbzbogs <dbzbogs16@gmail.com>
2019-05-06 15:15:36 +0700

    update

--- src/main/java/com/book/controller/OrderController.java
+++ src/main/java/com/book/controller/OrderController.java
@@ -8,6 +8,10 @@ import com.book.service.OrderDetailService;
 import com.book.service.OrderService;
 import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
@@ -25,6 +31,9 @@ public class OrderController {
 
     @Autowired
     OrderDetailService orderDetailService;
+
+    @Autowired
+    JavaMailSender javaMailSender;
 
     @RequestMapping("/order")
     public String order(@SessionAttribute(required = false) Cart cart,
@@ -55,6 +64,24 @@ public class OrderController {
             orderDetails.setQuantity(cart.getItems().get(i).getQuantity());
             orderDetails.setUnitPrice(cart.subTotal(cart.getItems().get(i).getBook().getId()));
             orderDetailService.addOrderDetail(orderDetails);
+        }
+//        SimpleMailMessage orderMail = new SimpleMailMessage();
+//        orderMail.setFrom("dbzbogs15@gmail.com");
+//        orderMail.setTo("15130169@st.hcmuaf.edu.vn");
+//        orderMail.setSubject("Order Book");
+//        orderMail.setText("Đơn hàng của bạn đã được xác nhận với Mã đơn hàng: " + trackingNumber);
+//        javaMailSender.send(orderMail);
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom("dbzbogs15@gmail.com");
+            helper.setTo("15130169@st.hcmuaf.edu.vn");
+            helper.setSubject("Order Book");
+            String text = "Đơn hàng của bạn đã được xác nhận với Mã đơn hàng: " + trackingNumber;
+            helper.setText(text + "<table><th>STT</th><th>Tên</th><tr><td>1</td><td>May mắn lần sau</td></tr></table>", true);
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
         }
         session.removeAttribute("cart");
         mm.addAttribute("message", "Bạn đã đặt hàng thành công vui lòng kiểm tra Email");
